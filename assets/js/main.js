7f34bd583a7eb08f92add418ed6e37dc0aff745b
Taimur Israr <taimurisrar806@gmail.com>
2022-01-07 00:52:59 +0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -243,3 +243,13 @@ anime.timeline({loop: true})
     delay: (el, i) => 100 + 30 * i
   });
 
+  $(function(){
+    var current = location.pathname;
+    $('#nav li a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
+})
