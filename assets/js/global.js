544e5d0f97e8af8d7d10ca14091faae0a5109167
Taimur Israr <taimurisrar806@gmail.com>
2022-01-07 22:07:25 +0500

    update

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,12 +1,3 @@
 const nav=`
-<nav class="nav-menu d-none d-lg-block">
-        <ul>
-          <li class="active"><a href="index.html">Home</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="solutions.html">Solutions</a></li>
-          <!-- <li><a href="careers.html">Careers</a></li>
-          <li><a href="blogs.html">Blogs</a></li> -->
-          <li><a href="contact.html">Contact</a></li>
-        </ul>
-      </nav>
+
 `;
